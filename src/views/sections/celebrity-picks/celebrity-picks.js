2fc5572b18c270ba6499be2fefbfe3a2e8e9907d
katie-lee-pointer <katie.lee9912@gmail.com>
2023-03-31 14:55:00 -0400

    page updates

--- src/views/sections/celebrity-picks/celebrity-picks.js
+++ src/views/sections/celebrity-picks/celebrity-picks.js
@@ -1,1 +1,28 @@
 import './celebrity-picks.scss'
+import AccessibleSwiper from '@scripts/components/accessible-swiper'
+
+(() => {
+  if (typeof window.customElements.get('celebrity-picks') !== 'undefined') { return }
+  window.customElements.define('celebrity-picks', class CelebrityPicks extends AccessibleSwiper { // eslint-disable-line
+    constructor () {
+      super()
+
+      this.slider = this.querySelector('[data-swiper-slider]')
+      this.next = this.querySelector('[data-swiper-next]')
+      this.prev = this.querySelector('[data-swiper-prev]')
+
+      this.options = {
+        ...this.options,
+        spaceBetween: 24,
+        slidesPerView: 'auto',
+        threshold: 10,
+        navigation: {
+          nextEl: this.next,
+          prevEl: this.prev
+        }
+      }
+
+      this.accessibleSwiper()
+    }
+  })
+})()
